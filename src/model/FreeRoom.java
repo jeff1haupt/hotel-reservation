ec1a89990c86556e3e35bde7070292936b8a60e5
jeff1haupt <jhaupt@southbendin.gov>
2021-04-28 06:03:04 -0400

    updated FreeRoom Class

--- src/model/FreeRoom.java
+++ src/model/FreeRoom.java
@@ -6,4 +6,9 @@ public class FreeRoom extends Room{
         public FreeRoom(String roomNumber, Double roomPrice, RoomType roomType) {
                 super(roomNumber, roomPrice, roomType);
         }
+
+        @Override
+        public String toString() {
+                return "Free room details - Room Number: " + roomNumber + "  SINGLE OR DOUBLE: " + roomType;
+        }
 }
